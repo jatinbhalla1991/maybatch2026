a81fc2eacc26174501bbf6a8f35efa2f243dfa0f
jatinbhalla <jatinbhalla18.jb@gmail.com>
2026-05-21 08:03:49 +0530

    lock example

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,17 @@
 
 
 
-resource "aws_s3_bucket" "abc" {
-    bucket = var.bucket_name
-}
+# resource "aws_s3_bucket" "abc" {
+#     bucket = var.bucket_name
+# }
 
 # resource "aws_s3_bucket" "abc1" {
 #     bucket = "jatin1234112121"
 # }
+
+
+
+resource "aws_ec2_instance" "my_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
+terraform {
+backend "s3" {
+  region = "us-east-1"
+  bucket = "my-terraform-state-1991"
+  key    = "devterraform.tfstate"
+}
+}
